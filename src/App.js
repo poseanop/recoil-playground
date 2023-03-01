cdedde38cd5e5181109c16c5c9a5567dc206ac66
poseanop <k3253hj@naver.com>
2023-03-01 17:34:46 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  atom,
+  selector,
+  RecoilRoot,
+  useRecoilValue,
+  useRecoilState,
+  useSetRecoilState,
+  selectorFamily,
+  waitForAll,
+  waitForNone,
+  useRecoilCallback,
+} from "recoil";
+import * as React from "react";
 
-function App() {
+const myDBQuery = ({ userID }) => {
+  console.log("요청", userID);
+  return new Promise((res, rej) => {
+    const table = new Array(10).fill(0).map((item, i) => {
+      return `${i}번째 유저`;
+    });
+    const index = (table.indexOf(userID) + 1) % 10;
+    const nextIndex = (table.indexOf(userID) + 2) % 10;
+    const nextIndex2 = (table.indexOf(userID) + 3) % 10;
+    setTimeout(() => {
+      res({
+        name: userID,
+        friendList: [table[index], table[nextIndex], table[nextIndex2]],
+      });
+    }, 1000);
+  });
+};
+
+const currentUserIDState = atom({
+  key: "CurrentUserID",
+  default: "0번째 유저",
+});
+
+const userInfoQuery = selectorFamily({
+  key: "UserInfoQuery",
+  get: (userID) => async () => {
+    const response = await myDBQuery({ userID });
+    if (response.error) {
+      throw response.error;
+    }
+    return response;
+  },
+});
+
+const currentUserInfoQuery = selector({
+  key: "CurrentUserInfoQuery",
+  get: ({ get }) => get(userInfoQuery(get(currentUserIDState))),
+});
+
+const friendsInfoQuery = selector({
+  key: "FriendsInfoQuery",
+  get: ({ get }) => {
+    const { friendList } = get(currentUserInfoQuery);
+    const friendLoadables = get(
+      waitForNone(friendList.map((friendID) => userInfoQuery(friendID)))
+    );
+    console.log(friendLoadables);
+    return friendLoadables
+      .filter(({ state }) => state === "hasValue")
+      .map(({ contents }) => contents);
+    // return friends;
+    // return friendList.map((friendID) => get(userInfoQuery(friendID)));
+  },
+});
+
+function CurrentUserInfo() {
+  const currentUser = useRecoilValue(currentUserInfoQuery);
+  const friends = useRecoilValue(friendsInfoQuery);
+  // const changeUser = useSetRecoilState(currentUserIDState);
+  const changeUser = useRecoilCallback(({ snapshot, set }) => (userID) => {
+    snapshot.getLoadable(userInfoQuery(userID)); // pre-fetch user info
+    set(currentUserIDState, userID); // change current user to start new render
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{currentUser.name}</h1>
+      <ul>
+        {friends.map((friend, idx) => (
+          <li key={idx} onClick={() => changeUser(friend.name)}>
+            {friend.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+function MyApp() {
+  return (
+    <RecoilRoot>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <CurrentUserInfo />
+      </React.Suspense>
+    </RecoilRoot>
+  );
+}
+
+export default MyApp;
